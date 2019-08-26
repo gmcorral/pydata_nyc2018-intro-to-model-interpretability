b67de3bf646ff1039ad7b52b34e440c0792f163d
Guillermo Menéndez <gmenende@amazon.com>
2019-08-26 13:33:36 +0200

    Added SageMaker instance lifecycle hook

---
+++ sagemaker-lifecycle.sh
@@ -0,0 +1,5 @@
+cd SageMaker/
+git clone https://github.com/klemag/pydata_nyc2018-intro-to-model-interpretability
+cd pydata_nyc2018-intro-to-model-interpretability/
+pip install -r requirements.txt
+
